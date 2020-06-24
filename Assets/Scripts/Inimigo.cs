5cc145ef35c8ca2355ca4a13c5dcbb3e0e9dfb44
Guilherme <guilherme1219@gmail.com>
2020-06-24 17:53:41 -0300

    Montagem do setup basico, e Player Movimentacao

---
+++ Assets/Scripts/Inimigo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inimigo : MonoBehaviour
+{
+    private PlayerController _PlayerController;
+
+    public Transform tfArma;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _PlayerController = FindObjectOfType(typeof(PlayerController)) as PlayerController;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tfArma.right = _PlayerController.transform.position - transform.position;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D playerRb;
+    public GameObject prefabShot;
+    public Transform armaPosicao;
+    public float velocidade;
+    public float velocidadeTiro;
+    public float fireRate;
+    private float nextShot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        playerRb.velocity = new Vector2(horizontal * velocidade, vertical * velocidade);
+
+        if (Input.GetButton("Fire1") && Time.time > nextShot)
+        {
+            nextShot = fireRate + Time.time;
+            Shot();
+        }
+    }
+
+    private void Shot()
+    {
+        GameObject shotTemp = Instantiate(prefabShot);
+        shotTemp.transform.position = armaPosicao.position;
+        shotTemp.GetComponent<Rigidbody2D>().velocity = new Vector2(0, velocidadeTiro);
+    }
+}
